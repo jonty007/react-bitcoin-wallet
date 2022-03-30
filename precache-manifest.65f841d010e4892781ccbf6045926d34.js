27a78dc9de84bbdc0e7070fcac09887aa35e328f
jonty007 <vivek.singh2994@gmail.com>
2022-03-30 10:30:37 +0530

    Updates

---
+++ precache-manifest.65f841d010e4892781ccbf6045926d34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d15cf05effd1a550715f1c5995b2125",
+    "url": "/react-bitcoin-wallet/index.html"
+  },
+  {
+    "revision": "fc1707858b2226ab6594",
+    "url": "/react-bitcoin-wallet/static/css/main.07326a6b.chunk.css"
+  },
+  {
+    "revision": "ee99324d568abb0d7f5e",
+    "url": "/react-bitcoin-wallet/static/js/2.ce6225b6.chunk.js"
+  },
+  {
+    "revision": "fc1707858b2226ab6594",
+    "url": "/react-bitcoin-wallet/static/js/main.a1347d2c.chunk.js"
+  },
+  {
+    "revision": "e3eb15e1566615e0de83",
+    "url": "/react-bitcoin-wallet/static/js/runtime-main.95b16242.js"
+  }
+]);
